137ed75c409db04e2e5983e6601388cfbc0dbdd5
Md. Shafiul Alam <31996183+ShafiulSumon@users.noreply.github.com>
2021-10-27 06:46:47 +0600

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,126 +1,78 @@
-#include <bits/stdc++.h>
+class LCA{
+public: 
+    int node,root;
 
-#define ll                 long long
-#define sf                 scanf
-#define pf                 printf
-#define nl                 printf("\n")
-#define endl               '\n'
-#define pb                 push_back
-#define rep(i,a,b)         for(int i=a; i<b; i++)
-#define repr(i,a,b)        for(int i=a; i>=b; i--)
-#define TEST               int Test;cin>>Test;for(int _t=1;_t<=Test;_t++)
-#define isValid(a,b,A,B)   (0<=a&&a<A) and (0<=b&&b<B)
+    // make these line global
 
-using namespace std;
+    // const int N = 1003;
 
-template <typename... T> void read(T &... args){ ((cin >> args), ...); }
+    // vector<int> vec[N];
+    // bool vis[N];
+    // int level[N], parent[N], SPT[N][(int)log2(N)+1];
 
-typedef pair<int,int> Pr;
-
-void yes(){ pf("Yes\n"); }
-void no() { pf("No\n"); }
-
-vector<int> vec[1003];
-bool vis[1003];
-int level[1003],parent[1003],SPT[1003][10];
-
-void BFS(){
-    memset(vis,false,sizeof(vis));
-    memset(level,0,sizeof(level));
-    memset(parent,-1,sizeof(parent));
-
-    vis[1] = true;
-    level[1] = 0;
-    queue<int> Q;
-
-    Q.push(1);
-
-    while(!Q.empty()){
-        int u = Q.front();
-        Q.pop();
-        for(auto v : vec[u]){
-            if(!vis[v]){
-                vis[v] = true;
-                level[v] = level[u] + 1;
-                Q.push(v);
-                parent[v] = u;
-            }
-        }
-    }
-}
-
-
-// SPT[i][j] = x;
-// 'x' is the 2^j -th parent of node 'i'
-void sparse_table(int node){
-    for(int i=1; i<=node; i++){
-        SPT[i][0] = parent[i]; // 2^0-th parent of i-th node
-    }
-    for(int j=1; (1<<j)<=node; j++){
-        for(int i = 1; i<=node; i++){
-            if(SPT[i][j-1]!=-1)
-                SPT[i][j] = SPT[SPT[i][j-1]][j-1];
-        }        
-    }
-}
-
-
-void Precal(int node){
-    // for leveling the nodes
-    BFS(); 
-
-    // store the parent's of every node 
-    // in 2's power
-    sparse_table(node); 
-}
-
-
-int find_LCA(int x, int y, int node){
-    if(level[x]<level[y]) swap(x,y); // now level of x is bigger or equal
     
-    for(int i = log2(node); i>=0; i--){
-        if(level[x]-(1<<i) >= level[y]){
-            x = SPT[x][i];
-        }
-    }
-    // now x and y are in same level
+    LCA(int node, int root){
+        this->node = node;
+        this->root = root;
 
-    if(x==y) return x; // overlapping node, so that's the LCA
+        // BFS for leveling the nodes
+        memset(vis,false,sizeof(vis));
+        memset(level,0,sizeof(level));
+        memset(parent,-1,sizeof(parent));
 
-    for(int i = log2(node); i>=0; i--){
-        if(SPT[x][i]!=-1 and SPT[x][i]!=SPT[y][i]){
-            x = SPT[x][i], y = SPT[y][i];
-        }
-    }
-    return SPT[x][0];
-}
+        vis[root] = true;
+        level[root] = 0;
+        queue<int> Q;
 
-int main()
-{   
-    int cas = 1;
-    int node,m,v,x,y,query;
-    TEST{
-        read(node);
-        rep(i,1,node+1){
-            read(m);
-            rep(j,0,m){
-                read(v);
-                vec[i].pb(v);
-                vec[v].pb(i);
+        Q.push(root);
+
+        while(!Q.empty()){
+            int u = Q.front();
+            Q.pop();
+            for(auto v : vec[u]){
+                if(!vis[v]){
+                    vis[v] = true;
+                    level[v] = level[u] + 1;
+                    Q.push(v);
+                    parent[v] = u;
+                }
             }
         }
+        // Sparse-table
 
-        Precal(node);
-
-        pf("Case %d:\n",cas++);
-        read(query);
-        while(query--){
-            read(x,y);
-            int ans = find_LCA(x,y,node);
-            if(ans==-1) ans = 1;
-            pf("%d\n",ans);
+        // store the parent's of every node 
+        // in 2's power
+        // SPT[i][j] = x;
+        // 'x' is the 2^j -th parent of node 'i'
+        for(int i=1; i<=node; i++){
+            SPT[i][0] = parent[i]; // 2^0-th parent of i-th node
         }
-        rep(i,1,node+1) vec[i].clear();
-    }   
-    return 0;
-}
+        for(int j=1; (1<<j)<=node; j++){
+            for(int i = 1; i<=node; i++){
+                if(SPT[i][j-1]!=-1)
+                    SPT[i][j] = SPT[SPT[i][j-1]][j-1];
+            }        
+        }
+    }
+
+
+    int findLCA(int x, int y){
+        if(level[x]<level[y]) swap(x,y); // now level of x is bigger or equal
+    
+        for(int i = log2(node); i>=0; i--){
+            if(level[x]-(1<<i) >= level[y]){
+                x = SPT[x][i];
+            }
+        }
+        // now x and y are in same level
+
+        if(x==y) return x; // overlapping node, so that's the LCA
+
+        for(int i = log2(node); i>=0; i--){
+            if(SPT[x][i]!=-1 and SPT[x][i]!=SPT[y][i]){
+                x = SPT[x][i], y = SPT[y][i];
+            }
+        }
+        return SPT[x][0];
+    }
+};
